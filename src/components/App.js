b521fafcd3598eb90ab4b5fc5f20de44c55bf416
ollaekahmed <ollaekahmed@gmail.com>
2019-09-10 23:44:38 +0200

    number page done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import {Router, Route, Switch } from "react-router-dom";
 
-class App extends React.Component{
-    render(){
-        return <div>App</div>
-    };
-}
+import Number from "./Number";
+import Wheel from "./Wheel";
+import Gift from "./Gift";
+import Error from "./Error";
+import history from "../history";
+
+const App = () => {
+    
+    return (
+        <div className="ui container">
+            <Router history={history}>
+                <div>
+                <Switch>
+                    <Route path="/" exact component={Number} />
+                    <Route path="/Wheel/:msisdn/:groupName" exact component={Wheel} />
+                    <Route path="/Gift" exact component={Gift} />
+                    <Route path="/Error" exact component={Error} />
+                </Switch>
+                </div>
+            </Router>
+        </div>
+    );
+
+};
 
 export default App;
---
+++ src/components/Wheel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Wheel = () => {
+    return <div>wheel</div>;
+};
+
+export default Wheel;
